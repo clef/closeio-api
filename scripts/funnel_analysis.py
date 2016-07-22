1d1d9c03945df02c0e24d075a1a120fb31021ce1
Jesse Pollak <jesse@pollak.io>
2016-07-21 18:25:36 -0700

    fix(funnel): add JSON and CSV formats

--- scripts/funnel_analysis.py
+++ scripts/funnel_analysis.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import datetime
+import json
 from datetime import timedelta
 import dateutil.parser
 import copy
@@ -10,6 +12,7 @@ from collections import (
     Counter,
     OrderedDict
 )
+from ordered_set import OrderedSet
 
 parser = argparse.ArgumentParser(description='Detect duplicates & merge leads (see source code for details)')
 parser.add_argument('--api-key', '-k', required=True, help='API Key')
@@ -18,14 +21,20 @@ parser.add_argument('--segment-default-value', required=False, help='A default v
 parser.add_argument('--weeks', type=int, required=False, help='The number of weeks to look at historically')
 parser.add_argument('--include-not-native', required=False, default=False, help='Whether we should include basic and business customers')
 parser.add_argument('--format', required=False, choices=['pretty', 'json', 'csv'], default='pretty', help='The format you want the data to be exported in')
+parser.add_argument('--funnel-format', required=False, choices=['raw', 'percentage'], default='raw', help='Whether you want the raw funnel or percentages')
 args = parser.parse_args()
 
 """
 Segment leads by created at and print out funnel analysis
 """
 
-class DefaultOrderedDict(defaultdict, OrderedDict):
-    pass
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, OrderedSet):
+            return list(obj)
+        else:
+            return super(DateTimeEncoder, self).default(obj)
+
 class OrderedCounter(Counter, OrderedDict):
     pass
 
@@ -49,7 +58,7 @@ def start_of_week(dt):
     return as_date - timedelta(days=as_date.weekday())
 
 def statuses_for_leads(leads, exclude=None):
-    count_by_status = Counter()
+    count_by_status = OrderedCounter()
     for lead in leads:
         if exclude and exclude(lead):
             continue
@@ -61,7 +70,7 @@ def segment_statuses_for_leads_by_property(leads, prop_getter=None, exclude=None
     for lead in leads:
         leads_by_property[prop_getter(lead)].append(lead)
 
-    statuses_by_property = dict()
+    statuses_by_property = OrderedDict()
     for prop, leads in leads_by_property.iteritems():
         statuses_by_property[prop] = statuses_for_leads(leads, exclude=exclude)
 
@@ -99,7 +108,7 @@ def get_leads_by_start_of_week(number_of_weeks=None):
         ordered = ordered[-number_of_weeks:]
     return OrderedDict(ordered)
 
-def funnel_count_at_each_status(lead_count_by_status):
+def funnel_at_each_status(lead_count_by_status):
     total_number_of_leads = sum(lead_count_by_status.values())
     remaining_statuses = copy.copy(status_order)
     status_funnel = OrderedCounter()
@@ -113,7 +122,20 @@ def funnel_count_at_each_status(lead_count_by_status):
     for status in status_exclude:
         status_funnel[status_order[0]] += lead_count_by_status.get(status, 0)
 
-    return status_funnel
+    percentage_status_funnel = dict()
+    for i, status in enumerate(status_order):
+        if i == 0:
+            percentage_status_funnel[status] = 100.0
+        else:
+            first = status_funnel[status_order[0]]
+            percentage_status_funnel[status] = float(status_funnel[status]) / float(first) * 100
+
+
+    return dict(
+        raw=status_funnel,
+        percentage=percentage_status_funnel
+    )
+
 
 def parse_segment_property_to_getter(prop, prop_default=None):
     selectors = prop.split(".")
@@ -137,67 +159,78 @@ class Formatter(object):
         else:
             return PrettyFormatter
 
-    def __init__(self, data):
+    def __init__(self, data, funnel_format):
         self.data = data
+        self.funnel_format = funnel_format
+
+    def format_step(self, step):
+        if self.funnel_format == "percentage":
+            return "{0:.1f}%".format(step)
+        else:
+            return str(step)
 
     def output(self):
         pass
 
 class PrettyFormatter(Formatter):
+
     def output(self):
-        for item in self.data:
-            print item.get('date')
+        for date in self.data.get('dates'):
+            headers = ["Step"]
+            columns = []
+            for segment in self.data.get('segments'):
+                funnel_on_date = self.data.get('funnels').get(segment).get(date)
+                if funnel_on_date:
+                    headers.append(segment)
+                    values = []
+                    for key in status_order:
+                        values.append(self.format_step(funnel_on_date.get(self.funnel_format)[key]))
+                    columns.append(values)
+
+            print date
             print "=" * 12
             print tabulate(
-                zip(
-                    status_order,
-                    *item.get('items')
-                ),
-                headers=["Step"] + item.get('headers'),
+                zip(status_order, *columns),
+                headers=headers,
                 tablefmt='grid'
             )
             print "\n"
 
 class JSONFormatter(Formatter):
     def output(self):
-        raise NotImplementedError("JSON output hasn't been implemented yet")
+        print json.dumps(data, indent=4, cls=CustomJSONEncoder)
 
 class CSVFormatter(Formatter):
     def output(self):
-        raise NotImplementedError("CSV output hasn't been implemented yet")
-        rows = defaultdict(list)
-        dates = []
-        for item in self.data:
-            dates.append(item.get('date').strftime('%D'))
-
-            zipped = zip(
-                *item.get('items')
-            )
-
-            for i, header in enumerate(item.get('headers')):
-                rows[header].append(
-                    zipped[i]
-                )
-
-        print ",".join(["Segment"] + dates)
-        for segment, data in rows.iteritems():
-            print segment, data
-
+        print ",".join(["Date"] + self.data.get('dates'))
+        for segment in self.data.get('segments'):
+            print segment
+            columns = []
+            for date in self.data.get('dates'):
+                funnel_on_date = self.data.get('funnels').get(segment).get(date)
+                if funnel_on_date:
+                    values = []
+                    for key in status_order:
+                        values.append(self.format_step(funnel_on_date.get(self.funnel_format)[key]))
+                    columns.append(values)
+                else:
+                    columns.append([self.format_step(0)] * len(status_order))
+            zipped = zip(status_order, *columns)
+            for item in zipped:
+                print ",".join(item)
 
 leads_by_start_of_week = get_leads_by_start_of_week(number_of_weeks=args.weeks)
-data =  []
-for date, leads in leads_by_start_of_week.iteritems():
-    headers = ["Total"]
-    data_items = []
+data = dict(dates=[], segments=OrderedSet(['Total']), funnels=defaultdict(OrderedDict))
 
+for date, leads in leads_by_start_of_week.iteritems():
+    date = date.isoformat()
     if args.include_not_native:
         exclude = None
     else:
         exclude = lambda x: x.get("custom").get("Plan") and x.get("custom").get("Plan") != "Native"
 
     total_statuses =  statuses_for_leads(leads, exclude=exclude)
-    total_funnel = funnel_count_at_each_status(total_statuses)
-    data_items.append(total_funnel.values())
+    data.get('funnels')['Total'][date] = funnel_at_each_status(total_statuses)
 
     if args.segment:
         segmented_statuses_dict = segment_statuses_for_leads_by_property(
@@ -207,14 +240,9 @@ for date, leads in leads_by_start_of_week.iteritems():
         )
 
         for segment, totals in segmented_statuses_dict.iteritems():
-            data_items.append(funnel_count_at_each_status(totals).values())
+            data.get('segments').add(segment)
+            data.get('funnels')[segment][date] = funnel_at_each_status(totals)
 
-        headers += segmented_statuses_dict.keys()
+    data.get('dates').append(date)
 
-    data.append(dict(
-        date=date,
-        headers=headers,
-        items=data_items
-    ))
-
-Formatter.get_formatter(args.format)(data).output()
+Formatter.get_formatter(args.format)(data, args.funnel_format).output()
